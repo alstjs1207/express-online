a268b8fad92a7d0d6629be6b0c56e8dbaddc2e42
alstjs1207 <alstjs1207@naver.com>
2021-05-14 02:04:37 +0900

    DB 처리 완성

--- models/Products.js
+++ models/Products.js
@@ -1,3 +1,4 @@
+const moment = require('moment');
 module.exports = (sequelize, DataTypes) => {
     const Products = sequelize.define('Products',
         {
@@ -7,5 +8,15 @@ module.exports = (sequelize, DataTypes) => {
             description : { type: DataTypes.TEXT }
         }
     );
+
+    // Products.prototype.dateFormat = (date) => {
+    //     return moment(date).format('YYYY년 MM 월 DD일')
+    // }
+
+    // 바로 return이면 생략가능
+    Products.prototype.dateFormat = (date) => (
+        moment(date).format('YYYY년 MM 월 DD일')
+    );
+
     return Products;
 }
